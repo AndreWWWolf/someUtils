304dec0e160c82f86d5ab3faa98f82fe7ea71700
AndreWWWolf <andrew.wolfeus@gmail.com>
2019-08-02 13:16:21 -0700

    power funciton added

--- utils.js
+++ utils.js
@@ -13,3 +13,11 @@ console.log(product(6, 3));
 console.log(product(18, 2));
 console.log(product(60, 3));
 console.log(product(21, 7));
+
+function power(x, y) {
+    return x**y;
+}
+console.log(power(6, 3));
+console.log(power(18, 2));
+console.log(power(60, 3));
+console.log(power(21, 7));
