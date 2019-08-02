8d2f7c79ef927f3bb022d324d8e5a5393e4ccac3
AndreWWWolf <andrew.wolfeus@gmail.com>
2019-08-02 13:15:35 -0700

    product && divide functions added

--- utils.js
+++ utils.js
@@ -6,4 +6,10 @@ console.log(divide(18, 2));
 console.log(divide(60, 3));
 console.log(divide(21, 7));
 
-
+function product(x, y) {
+    return x - y;
+};
+console.log(product(6, 3));
+console.log(product(18, 2));
+console.log(product(60, 3));
+console.log(product(21, 7));
